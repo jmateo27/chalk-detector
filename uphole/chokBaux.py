c7a8a9b2ec46fd228d00e6b2ca50fe2a5e020a2b
unknown <justin27mateo@gmail.com>
2025-05-12 16:29:26 -0700

    Uphole test ongoing

--- uphole/chokBaux.py
+++ uphole/chokBaux.py
@@ -13,15 +13,14 @@ class ChokBaux:
         self.adc        = ADC_Reader(ADC1_PIN, ADC2_PIN)
 
     def collectData(self):
-        self.dac.begin()
-        self.led.LED_on()
         time.sleep(3)
-    # with open('data.txt', 'w') as file:
-        time.sleep(0.1)
-        v = self.adc.measure_voltage_drop()
-        print("Voltage Reading = %f" % (v))
-        # file.write("%d\t%d\t%d\t%d\n" % (x, r, g, b))
-        time.sleep(2)
+        while True:
+        # with open('data.txt', 'w') as file:
+            time.sleep(0.1)
+            v = self.adc.measure_voltage_drop()
+            print("Voltage Reading = %f" % (v))
+            # file.write("%d\t%d\t%d\t%d\n" % (x, r, g, b))
+            time.sleep(2)
 
 if __name__ == "__main__":
     chokBaux = ChokBaux()
