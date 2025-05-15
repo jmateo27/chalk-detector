f470e6b0c786d04cf4548b74d038f4e5ea4dc67a
MATEO Justin <justin.mateo@vinci-construction.com>
2025-05-15 09:15:37 -0700

    Decreased measurement/output latency and made it more power efficient

--- downhole/main.py
+++ downhole/main.py
@@ -1,6 +1,6 @@
 from rgb import RGB_Sensor
 from led import LED
-from adcReader import ADC_Reader
+# from adcReader import ADC_Reader
 from dac4to20 import DAC_4to20
 
 import time
@@ -11,12 +11,13 @@ PERIPHERAL_FREQ             = 100000
 RGB_SENSOR_SDA_PIN          = 16
 RGB_SENSOR_SCL_PIN          = 17
 LED_PWM_PIN                 = 21
-ADC1_PIN                    = 28
-ADC2_PIN                    = 27
+# ADC1_PIN                    = 28
+# ADC2_PIN                    = 27
 DAC_SDA_PIN                 = 18
 DAC_SCL_PIN                 = 19
 
-MEASUREMENT_LATENCY_SECS    = 2.0
+INIT_WAIT_SECS              = 2
+MEASUREMENT_LATENCY_MS      = 10
 
 RED     = 1
 GREEN   = 2
@@ -28,23 +29,23 @@ class Chalk_Detector:
         self.rgbSensor  = RGB_Sensor(RGB_SENSOR_SCL_PIN, RGB_SENSOR_SDA_PIN, PERIPHERAL_FREQ, 0)
         self.dac        = DAC_4to20(DAC_SCL_PIN, DAC_SDA_PIN, PERIPHERAL_FREQ, 1)
         self.led        = LED(LED_PWM_PIN)
-        self.adc        = ADC_Reader(ADC1_PIN, ADC2_PIN)
+        # self.adc        = ADC_Reader(ADC1_PIN, ADC2_PIN)
     
     def main(self):
         self.dac.begin()
         self.led.LED_on()
     
         while True:
-            print('Red:', self.rgbSensor.read_colour_raw(RED))
-            print('Green:', self.rgbSensor.read_colour_raw(GREEN))
-            print('Blue:', self.rgbSensor.read_colour_raw(BLUE))
+            # print('Red:', self.rgbSensor.read_colour_raw(RED))
+            # print('Green:', self.rgbSensor.read_colour_raw(GREEN))
+            # print('Blue:', self.rgbSensor.read_colour_raw(BLUE))
             self.dac.output(self.rgbSensor.read_colour_mA(BLUE))
-            time.sleep(0.1)
-            self.adc.print_voltage_drop()
-            print('')
-            time.sleep(MEASUREMENT_LATENCY_SECS)
+            # time.sleep(0.1)
+            # self.adc.print_voltage_drop()
+            # print('')
+            time.sleep_ms(MEASUREMENT_LATENCY_MS)
 
 if __name__ == "__main__":
-    time.sleep(5)
+    time.sleep(INIT_WAIT_SECS)
     chalkDetector = Chalk_Detector()
     chalkDetector.main()
